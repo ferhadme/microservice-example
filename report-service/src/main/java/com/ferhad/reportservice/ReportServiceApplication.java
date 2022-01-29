bbb6accf0de15900252f89c4467e8562a9899b49
ferhadme <frhdmehdiyev@gmail.com>
2022-01-29 13:04:42 +0400

    eureka client enabled for report service

--- report-service/src/main/java/com/ferhad/reportservice/ReportServiceApplication.java
+++ report-service/src/main/java/com/ferhad/reportservice/ReportServiceApplication.java
@@ -2,8 +2,10 @@ package com.ferhad.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
